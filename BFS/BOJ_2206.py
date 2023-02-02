a8b6094a0c0b1bf13ca436d74e6c31cc52d23519
km-so-yeon <yeonsokm@gmail.com>
2023-02-03 00:00:10 +0900

    [update] BOJ_2206 메모리 초과

--- BFS/BOJ_2206.py
+++ BFS/BOJ_2206.py
@@ -6,13 +6,14 @@ input = sys.stdin.readline
 
 def bfs() :
     q = deque()
-    q.append((0, 0, 1, 0)) #x, y, 몇번째 방문인지, 통과여부(이미 했음->불가능일 경우 1, 아니면 0)
+    q.append([0, 0, 0]) #x, y, 통과여부(이미 했음->불가능일 경우 1, 아니면 0)
+    visited[0][0] = 1
 
     while q :
-        x, y, n, yn = q.popleft()
+        x, y, yn = q.popleft()
 
         if x == M-1 and y == N-1 :
-            return n
+            return visited[y][x]
 
         # 상하좌우 추가
         for i in range(4) :
@@ -29,10 +30,10 @@ def bfs() :
                     continue
                 # 처음 한번은 벽 통과
                 else :
-                    q.append((nx, ny, n + 1, 1))
+                    q.append([nx, ny, 1])
                     continue
 
-            q.append((nx, ny, n + 1, yn))
+            q.append([nx, ny, yn])
 
     # 성공하지 못했을 경우
     return -1
@@ -43,6 +44,9 @@ N, M = map(int, input().split())
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 map = []
+
+# 방문 기록을 따로 만들기
+visited = [[0 for _ in range(M)] for _ in range(N)]
 
 for i in range(N) :
     map.append(input().rstrip())
